fa08cfcbb86d4265a4016f3911bb4d5f7589030f
Mahmudul Hasan Rifat <mahmudulhasanrifat220@gmail.com>
2024-10-02 22:00:32 +0600

    video category done

--- script/vedio.js
+++ script/vedio.js
@@ -12,45 +12,37 @@ const showBtn = (categories) =>{
     const btnDiv = document.getElementById('btnDiv');
 
     categories.forEach(item => {
-        const button = document.createElement('button');
-        button.classList.add('btn');
-        button.innerText = item.category;
 
-        btnDiv.appendChild(button);
+        const btnContainer = document.createElement('div');
+        btnContainer.innerHTML = `
+        <button onclick= 'loadCategoryVideo(${item.category_id})' class = 'btn'>${item.category}</button>
+        `
+        
+
+        btnDiv.appendChild(btnContainer);
     });
+};
+
+const loadCategoryVideo = (id) =>{
+    fetch(`https://openapi.programming-hero.com/api/phero-tube/category/${id}`)
+    .then(res => res.json())
+    .then(data => showVideo(data.category))
 }
 
-const demo = {
-    "category_id": "1001",
-    "video_id": "aaaa",
-    "thumbnail": "https://i.ibb.co/L1b6xSq/shape.jpg",
-    "title": "Shape of You",
-    "authors": [
-        {
-            "profile_picture": "https://i.ibb.co/D9wWRM6/olivia.jpg",
-            "profile_name": "Olivia Mitchell",
-            "verified": ""
-        }
-    ],
-    "others": {
-        "views": "100K",
-        "posted_date": "16278"
-    },
-    "description": "Dive into the rhythm of 'Shape of You,' a captivating track that blends pop sensibilities with vibrant beats. Created by Olivia Mitchell, this song has already gained 100K views since its release. With its infectious melody and heartfelt lyrics, 'Shape of You' is perfect for fans looking for an uplifting musical experience. Let the music take over as Olivia's vocal prowess and unique style create a memorable listening journey."
-}
 
 const loadVideo = () => {
     fetch('https://openapi.programming-hero.com/api/phero-tube/videos')
     .then((res) => res.json())
     .then((data) => showVideo(data.videos))
-}
+};
 
 const showVideo = (videos) => {
 
     const videoDivContainer = document.getElementById('videoDivContainer');
+    videoDivContainer.innerHTML = '';
 
     videos.forEach((video) => {
-        console.log(video);
+        // console.log(video);
 
         const card = document.createElement('div');
         card.classList = 'card card-compact';
